940314da856ff0d7ec9063061fbbfaf135c34d96
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-07-11 11:36:21 -0400

    one num is off, code is almost right

--- grand_bash_test.rb
+++ grand_bash_test.rb
@@ -6,8 +6,19 @@ class Grand_Bash_test < Minitest::Test
 		assert_equal(0, array_empty.count)
 	end
    def test_the_winning_numbers
-   my_ticket = "6969"
-	winning_nums = ["1234", "5678", "9101", "1121", "3141", "6969"]
-   	assert_equal(["6969"],array_winning(my_ticket, winning_nums))
+   my_ticket = "1492"
+	winning_nums = ["1234", "5678", "9101", "1121", "3141", "1492"]
+   	assert_equal(["1492"],array_winning(my_ticket, winning_nums))
    end
+   def test_how_close_are_my_nums
+   	my_ticket = "5679"
+   	winning_nums = ["1234", "5678", "9101", "1121", "3141", "1496"]
+   	assert_equal(0, array_one_num_off(my_ticket, winning_nums))
+   end
+   def test_how_close_are_the_second_nums
+   	my_ticket = "5679"
+   	winning_nums = ["1234", "5678", "9101", "1121", "3141", "1496"]
+   	assert_equal(3, array_second_num_off(my_ticket, winning_nums))
+   end
+
 end
--- grandbash.rb
+++ grandbash.rb
@@ -1,6 +1,9 @@
 def array_empty()
 	x = []
 end 
+ my_ticket = "1492"
+	winning_nums = ["1234", "5678", "9101", "1121", "3141", "1492"]
+
 def array_winning(my_ticket, winning_nums)
 	matches = []
 	winning_nums.each do |winning_nums|
@@ -11,5 +14,48 @@ def array_winning(my_ticket, winning_nums)
 		matches
 
 end
+def array_one_num_off(my_ticket, winning_nums)
+	num_of_correct_nums = 0
+	if my_ticket[0] == winning_nums[0][0]
+	num_of_correct_nums+1
+	else 0 
+	end
+	if my_ticket[1] == winning_nums[0][1]
+		num_of_correct_nums+1
+	else 0
+	end 
+	if my_ticket[2] == winning_nums[0][2]
+		num_of_correct_nums+1
+	else 0
+	end
+	if my_ticket[3] == winning_nums[0][3]
+	else 0 
+	end
+	#winning_nums.each do |number|
+	#	(number.first.split(//) & my_ticket.split(//)).size >= 3
+	#end
+	#my_ticket_arr = my_ticket.chars
+	#winning_nums.any? {|w| w.chars & my_ticket_arr).size == 3 }
+end 
+def array_second_num_off(my_ticket, winning_nums)
+	num_of_correct = 0 
+	if my_ticket[0] == winning_nums[1][0]
+		num_of_correct+1
+	else 0 
+	end
+	if my_ticket[1] == winning_nums[1][1]
+		num_of_correct+1
+	else 0
+	end
+	if my_ticket[2] == winning_nums[1][2]
+	else 0
+	end
+	if my_ticket[3] == winning_nums[1][3]
+		num_of_correct+1
+	else 0 
+	end
+end
+
+
 
 	
